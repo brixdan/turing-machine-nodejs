c9e9b66435163741235a49d1c93b7629cfa0128a
brixdan <brixdan@gmail.com>
2021-04-20 01:36:10 +0300

    Negative indexes, interesting mixin

--- TMs/mixin.js
+++ TMs/mixin.js
@@ -2,12 +2,12 @@
 
 const program = {
     q0: {
-        0: {w:1,m:R,n:q1},
-        1: {w:0,m:R,n:q0},
+        0: {w:0,m:R,n:q1},
+        1: {w:B,m:L,n:q0},
         B: {w:0,m:L,n:q1}
     },
     q1: {
-        0: {w:1,m:L,n:q0},
+        0: {w:1,m:L,n:q1},
         1: {w:1,m:R,n:halt},
         B: {w:0,m:R,n:q0}
     },
--- engine.js
+++ engine.js
@@ -17,7 +17,6 @@ var tm = function (
     if (typeof script === "string") (name = script, script = require("./TMs/" + script));
 
     while ((step < 200) && (q in script || typeof q === "function")) {
-        if (v) console.log(`${name}: step ${step}:`, ...tape.slice(0, p), tape[p] + '\'' + tape.slice(p + 1), q);
         if (typeof q === "function") {
             tape = q(tape, q0, p)(tm);
             if (tape.halt) break;
@@ -29,6 +28,7 @@ var tm = function (
             break
         }
         with (script[q][tape[p] ?? "B"]) {
+            if (v) console.log(`${name}: step ${step}:`, ...show(tape,p),q,w,m,n);
             tape[p] = w
             q = n
             switch (m) {        // move to next p-position
@@ -47,42 +47,20 @@ var tm = function (
     if (v) console.log(`${name}: step ${step}:`, ...tape);
     return tape;
 }
-//-------------------------------
-// First task
-// var tape = [0,1];
-// for(let i = 0; i<4;i++){
-// tape = tm("increment", tape);
-// console.log("out:", ...tape)
-// }
-//-------------------------------
-// var tape = [0,1,B,0,1];
-// tape = tm("sum", tape, tape.length);
-// console.log("out:", ...tape, "limit = ",tape.limit)
-// Task: describe grow-machine from halt-problem point of view
 
-// var tape = [0,1]; // stop at 3
-// //var tape = [0]; // stops at 5
-// var tape = [1,0]; // stops at 6
-// var tape = [1,1,1]; // unstops
-// var tape = [1,0,0,1]; // unstop
-// var tape = [1,0,1]; // stops at 4
-// var tape = [1,0,0,0,1]; // stops at 8
-// var tape = [1,0,0,0,0,1]; // unstop
-// var tape = [1]; // stop at 5
-// var tape = [1,1]; // unstop
-// var tape = [1,0,0,0,0,0,1]; // stop 12
-// var tape = [1,0,0,1,0,0,1]; // unstop
-// var tape = [1,0,0,0,0,0,0,0,0,0,0,0,1]; // stop
-// var tape = [1,0,0,0,0,0,0,1,0,0,0,0,1]; // unstop
-// var tape = [1,0,0,0,0,0,1,1,0,0,0,0,1]; // unstop
-// var tape = [0,0,0,0,0,0,0,0,0,0,1]; // unstop
-// tape = tm("grow", tape,60);
-// console.log("out:", ...tape, "limit = ",tape.limit)
-//----------------- Drift -------------
-// var tape = [1,0,1,1,0];
-// tape = tm("drift", tape);
-// console.log("out:", ...tape, "limit = ", tape.limit)
-// --------------------------------
+function show(ar,p) {
+    let out = []
+    for (let i = 0; i < 20; i++) {
+        out[i] = ar[i - 10] ??'_';
+    }
+    out = [...out.slice(0,10),'*',...out.slice(10)]
+    if (p !== undefined) {
+        p = p + 10;
+        out.splice(p + 2, 0, "'")
+    }
+    return out
+}
+// -----------------MIXIN---------------
 // let res = {}
 // let tape = [0]
 // let temp = [0]
@@ -95,21 +73,10 @@ var tm = function (
 //     res[s] = tape.limit;
 // }
 // console.log("result:",res);
-//-------------------------------
-// Допустим существует алгоритм, как угодно много состояний, который может лишь "поглядев"
-// на число, сказать кончит на нём или нет. Тогда можно предположть, что он будет идти
-// только вперёд и проходить каждый бит.
-// Идея - такого не может быть. Возьмем первое совпадение состояний на конце супербольшого
-// числа. Допустим что grow даёт одинаковый на них результат ибо если разный, то умник-то
-// в одном состоянии при одном входе должен дать одно и то же. Однако мы можем "вырастить"
-// оба числа так, что они разойдуться. Пример: ускользает пока
-var tape1 = [1,0,1,1,0,0,1,1,1,0,0,0,1,1,1,1,0,0,0]; // initial совпадение stop
-var tape2 = [1,0,1,1,0,0,1]; //
 
-tape1 = [1,0,0,0,0,0,0]; // initial совпадение stop
-tape2 = [1,0,0,0,0,0,1]; // ускользает
+var tape = [0,0,B]; // interest
+tape=[0,0,B,0]
 
-tape = tm("grow", tape1,60,q0,0,false);
-console.log("out1:", ...tape1, "limit = ",tape1.limit)
-tape = tm("grow", tape2,60,q0,0,false);
-console.log("out1:", ...tape2, "limit = ",tape2.limit)
+tape = tm("mixin", tape,100,q0,0,true);
+console.log("out:", ...show(tape), "limit = ",tape.limit)
+
