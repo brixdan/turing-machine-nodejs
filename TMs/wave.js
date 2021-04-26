737ad9581a0a0beefd56b3ff3bb82587467c5ffb
brixdan <brixdan@gmail.com>
2021-04-27 00:27:36 +0300

    Wave starts

---
+++ TMs/wave.js
@@ -0,0 +1,15 @@
+// move to the right
+
+const program = {
+    q0: {
+        0: {w:1,m:R,n:q1},
+        1: {w:0,m:R,n:q0},
+        B: {w:0,m:0,n:q0}
+    },
+    q1: {
+        0: {w:1,m:L,n:q0},
+        1: {w:1,m:R,n:q1},
+        B: {w:B,m:R,n:halt}
+    },
+}
+module.exports = program;
