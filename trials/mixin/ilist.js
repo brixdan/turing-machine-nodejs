21c0ed73887cfce7750aa48a767eaf31d991d4c1
brixdan <brixdan@gmail.com>
2021-04-27 09:04:35 +0300

    Ilist-

--- trials/mixin/ilist.js
+++ trials/mixin/ilist.js
@@ -3,13 +3,16 @@ const { tm } = require('../../engine')
 
 let res = {}
 let tape = [0]
-let temp = [0]
-let s = '';
-for (let i = 0; i < 10; i++) {
-    tape = tm("increment", temp);
-    temp = [...tape];
-    tm("grow", tape,60);
-    s = temp.toString()
-    res[s] = tape.limit;
-}
-console.log("result:",res);
+let temp = [1,1,1]
+// let s = '';
+// for (let i = 0; i < 10; i++) {
+//     tape = tm("increment", temp);
+//     temp = [...tape];
+//     tm("grow", tape,60);
+//     s = temp.toString()
+//     res[s] = tape.limit;
+// }
+// console.log("result:",res);
+
+tape = tm("increment", temp);
+console.log(tape)
