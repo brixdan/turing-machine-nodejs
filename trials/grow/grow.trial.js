4c725be3505dcc1670c22b41a31039d952d9fd20
brixdan <brixdan@gmail.com>
2021-04-29 12:28:33 +0300

    resolver +

--- trials/grow/grow.trial.js
+++ trials/grow/grow.trial.js
@@ -4,9 +4,14 @@ var tape;
     tape = [1,1,1] // infinity
     let step = tm("grow", tape, 100, q0, 0, true);
     console.log("step = ", step);
-})();
+});
 (function () {
     tape = [0,0,0] // @6
     let step = tm("grow", tape, 100, q0, 0, true);
     console.log("step = ", step);
 });
+(function () {
+    tape = [1,1] // infinity
+    let step = tm("grow", tape, 100, q0, 2, true);
+    console.log("step = ", step);
+})();
---
+++ trials/grow/resolver.grow.js
@@ -0,0 +1,70 @@
+const {tm} = require('../../engine')
+var tape;
+(function () {
+    tape = [0,0,0,1] //
+    let step = tm("grow", tape, 100, q0, 0, true);
+    console.log("step = ", step);
+});
+tape = [0,0,0,1] // infinity
+function resolve(tape, p, q) {
+    let stop = "stop";
+    if ((p > 1) && (tape[p] === undefined)) {
+        if (q === q0) {
+            switch (tape[p - 2] + "" + tape[p - 1]) {
+                case "00":
+                    stop = "infinity" // check
+                    break;
+                case "10":
+                    stop = "stop" // check
+                    break;
+                case "01":
+                    stop = "infinity" // check
+                    break;
+                case "11":
+                    stop = "infinity"
+                    break;
+            }
+        }
+        if (q === q1) {
+            stop = "stop"
+        }
+    }
+    if (tape[p] === undefined)
+        return stop;
+    switch (q) {
+        case q0:
+            switch (tape[p]) {
+                case 0:
+                    q = q1;
+                    tape[p] = 1;
+                    break;
+                case 1:
+                    q = q0;
+                    tape[p] = 0;
+                    break
+            }
+            ;
+            break;
+        case q1:
+            switch (tape[p]) {
+                case 0:
+                    q = q0;
+                    tape[p] = 1;
+                    break;
+                case 1:
+                    q = q1;
+                    tape[p] = 1;
+                    break
+            }
+            ;
+            break;
+    }
+    console.log("tape = " + tape, "p = ", ++p, "q = ", q)
+    return resolve(tape, p, q);
+}
+
+let res = resolve(tape, 0, q0)
+
+//    let step = tm("grow", tape, 100, q0, 0, true);
+console.log("res = ", res, "tape = ", tape);
+
