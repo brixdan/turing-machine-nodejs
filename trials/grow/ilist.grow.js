63f6707bb170c083a30ddc9a181ebe7e45664465
brixdan <brixdan@gmail.com>
2021-05-03 09:44:30 +0300

    Bug in resolver '0,0': 'infinity  stop',

--- trials/grow/ilist.grow.js
+++ trials/grow/ilist.grow.js
@@ -15,4 +15,17 @@ let temp = [0];
         tm("increment", temp, 1000,q0,0,false);
     }
     console.log("result:", res);
-})()
+});
+(function () {
+    let s = '';
+    for (let i = 0; i < 100; i++) {
+        s = temp.toString()
+        tape = [...temp];
+        tape1 = [...temp];
+        let total = tm("grow", tape, 100, q0, 0, false);
+        res[s] = total + "  " + resolver(tape1, 0, q0);
+        //tm("increment", temp, 1000,q0,0,false);
+        temp.push(Math.floor(Math.random() * 2));
+    }
+    console.log("result:", res);
+})();
