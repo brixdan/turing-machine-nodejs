a9922ef7b26fed35bb0b2d24f47f4fd4ae731d5f
brixdan <brixdan@gmail.com>
2021-05-28 18:40:50 +0300

    Not diverse: seed = [1,0,1,1,0,1,1,0,1 ]; seed1 = [1,0,1,1,0,1,1,0,1,1,0,1 ];

--- trials/rgrow/parallel.rgrow.js
+++ trials/rgrow/parallel.rgrow.js
@@ -13,47 +13,55 @@ let seed1 = seed.concat(ruler); // both stop by eventually diverge
 function check(seed,seed1) {
     let temp = [0];
     let res = {};
-    while ( i < 300) {
+    while ( i < 500) {
 
         let tape = [...seed.concat(temp)];
         let tape1 = [...seed1.concat(temp)];
-        total = tm("rgrow", tape, 300, q0, 0, false);
-        total1 = tm("rgrow", tape1, 300, q0, 0, false);
+        total = tm("rgrow", tape, 500, q0, 0, false);
+        total1 = tm("rgrow", tape1, 500, q0, 0, false);
         i++;
         if (typeof total !== typeof total1) {
             res[i] = [...seed.concat(temp)] + "  " + [...seed1.concat(temp)] + "  " + total + "  " + total1;
             break;
         }
-        tm("increment", temp, 300,q0,0,false);
+        tm("increment", temp, 500,q0,0,false);
     }
     return Object.keys(res).length;
 }
 
-// console.log(check(seed,seed1));
+console.log(check(seed,seed1));
 
 
-function checkMany (seed,seed1) {
-    let tape = [...seed];
-    let tape1 = [...seed1];
+function checkMany (seed) {
+    let tape, tape1;
+    let i = 0;
+    let r,r1;
     while ( i < 100) {
-
-        tape1 = [...tape1.concat(seed)]
+        i++;
+        r = Math.ceil(Math.random()*3) + 1;
+        r1 = Math.ceil(Math.random()*3) + 1;
+        if (r === r1) r1++;
+        console.log(' r = ', r,' r1 = ',r1)
+        tape = Array(r).fill(seed).flat();
+        tape1 = Array(r1).fill(seed).flat();
         if (check(tape,tape1) === 0) return i + " tape = " + tape +
             " tape1 =  " + tape1;
 
     };
     return "All pass";
 }
-console.log(checkMany(seed,seed1));
-
+// console.log(checkMany([1,0,1]));
+seed = [1,0,1,1,0,1,1,0,1 ];
+seed1 = [1,0,1,1,0,1,1,0,1,1,0,1 ];
 (function () {
-    while ( i < 100) {
+    temp = [0]
+    while ( i < 1000) {
 
         tape = [...seed.concat(temp)];
         tape1 = [...seed1.concat(temp)];
         // console.log("income tape = ", ...tape);
-        total = tm("rgrow", tape, 300, q0, 0, false);
-        total1 = tm("rgrow", tape1, 300, q0, 0, false);
+        total = tm("rgrow", tape, 500, q0, 0, false);
+        total1 = tm("rgrow", tape1, 500, q0, 0, false);
         //res[i++] = [...seed.concat(temp)] + "  " + total;
         i++;
         if (typeof total !== typeof total1) {
@@ -67,8 +75,8 @@ console.log(checkMany(seed,seed1));
         // console.log("out tape = ", ...tape, "total = ", total)
         if (temp.length === 0) temp = [0]; else
            // temp = temp.concat(ruler);
-        tm("increment", temp, 100,q0,0,false);
+        tm("increment", temp, 1000,q0,0,false);
            // temp.push(Math.floor(Math.random() * 2));
     }
     console.log("result:", res);
-});
+})();
