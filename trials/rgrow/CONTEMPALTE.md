78958efdcfad227cf8a07cd26a81034abed55a4e
brixdan <brixdan@gmail.com>
2021-06-05 07:13:33 +0300

    base1_size40_depth200

--- trials/rgrow/CONTEMPALTE.md
+++ trials/rgrow/CONTEMPALTE.md
@@ -2,3 +2,62 @@
 - Fractal is a structure that repeats low scale pattern on larger scales, because initial information is limited, no source of creativity exists to produce something new on big time spans
 - Deterministic process that didn't halt on a scale of a few hundred times larger than seed number ends up to cycle/oscillate/loop which is easy to identify in any particular case and to prove mathematically, that the proccess repeats itself and will never halt in future
 - Every state of TM potentially designates a double ternary operator. So TM with a couple of dozens of states represents an algorithm of complexity, superior to real life. Not every big project has that number of ternaries. Low on memory but high on logic. Memory comes from tape.
+- Interesting: already on size = 30 champion almost exeeds the allowed depth:
+            champ =  {
+              steps: 2035,
+              tape: 0, 0, 1, 1, 0,
+                1, 1, 0, 1, 0,
+                0, 1
+
+            }
+depth = 200 * length/12 = 2400 - quite same numbers
+- with very little possibility to get mistaked,
+we have a group of 28 first numbers that diverge of each other:
+[
+  "0",
+  "1",
+  "01",
+  "11",
+  "001",
+  "101",
+  "011",
+  "1001",
+  "0101",
+  "1101",
+  "0011",
+  "1111",
+  "10001",
+  "11001",
+  "00101",
+  "11101",
+  "01011",
+  "11011",
+  "11111",
+  "100001",
+  "110001",
+  "101001",
+  "111001",
+  "110101",
+  "111101",
+  "100011",
+  "101011",
+  "110111",
+  {
+    "steps": 2035,
+    "tape":
+      0,
+      0,
+      1,
+      1,
+      0,
+      1,
+      1,
+      0,
+      1,
+      0,
+      0,
+      1
+
+  }
+]
+
--- trials/rgrow/infinite.unequal.row.js
+++ trials/rgrow/infinite.unequal.row.js
@@ -1,23 +1,23 @@
-const { tm } = require('../../engine');
+const {tm} = require('../../engine');
 // Only deal with strings
 
 let memo = {}; // to remember results
 
-function compare(seed,seed1, depth, aheads) {
+function compare(seed, seed1, depth, aheads) {
     let temp = [];
     let res = {};
     let total = 0;
     let total1 = 0;
     let i = 0;
-    let cycles = aheads * Math.max(seed.length,seed1.length);
+    let cycles = aheads * Math.max(seed.length, seed1.length);
 
-    while ( i < cycles) {
-        let t = temp?seed.toArray().concat(temp):seed.toArray();
-        let t1 = temp?seed1.toArray().concat(temp):seed1.toArray();
+    while (i < cycles) {
+        let t = temp ? seed.toArray().concat(temp) : seed.toArray();
+        let t1 = temp ? seed1.toArray().concat(temp) : seed1.toArray();
 
         let tape = [...t];
         total = (memo[tape.toLeftString()]) ??
-        tm("rgrow", tape, depth * tape.length, q0, 0, false);
+            tm("rgrow", tape, depth * tape.length, q0, 0, false);
         memo[[...t].toLeftString()] = total;
         if (typeof total === 'number' && total > memo['champ'].steps) {
             memo['champ'].steps = total;
@@ -25,7 +25,7 @@ function compare(seed,seed1, depth, aheads) {
         }
         let tape1 = [...t1];
         total1 = (memo[tape1.toLeftString()]) ??
-        tm("rgrow", tape1, depth * tape.length, q0, 0, false);
+            tm("rgrow", tape1, depth * tape.length, q0, 0, false);
         memo[[...t1].toLeftString()] = total1;
         if (typeof total1 === 'number' && total1 > memo['champ'].steps) {
             memo['champ'].steps = total1;
@@ -37,16 +37,17 @@ function compare(seed,seed1, depth, aheads) {
             break;
         }
         temp = temp ?? [0];
-        tm("increment", temp, 100, q0,0,false);
+        tm("increment", temp, 100, q0, 0, false);
     }
     console.log("res = ", res);
     return Object.keys(res).length;
 }
+
 // let seed = '0' // Array(1).fill([1,0,1]).flat(); // +
 // let seed1 = "101"; // seed.concat([]); // both stop by eventually diverge
 // console.log(compare(seed,seed1));
 
-function compareMany (arr = ["0"], nuvo, depth, aheads) {
+function compareMany(arr = ["0"], nuvo, depth, aheads) {
     for (let item of arr) {
         if (arr.indexOf(nuvo) > -1) return 0;
         if (compare(item, nuvo, depth, aheads) === 0) {
@@ -55,6 +56,7 @@ function compareMany (arr = ["0"], nuvo, depth, aheads) {
     }
     return 1;
 }
+
 // console.log(compareMany(["0", "1", "01", "101"], "101101", 40));
 
 // Size is a gauge for lookup of a new candidates for divergent group.
@@ -64,49 +66,48 @@ function compareMany (arr = ["0"], nuvo, depth, aheads) {
 // went into loop. Must depend on number size.
 // Aheads is a gauge for a number of lookups in the future to identify
 // divergency in the process of parallel growth. Fraction to number size.
-function buildDivGroup (arr = ['0'], size = 10,
-    depth = 500, aheads = 2 ) {
+function buildDivGroup(arr = ['0'], size = 10,
+                       depth = 500, aheads = 2) {
     let nuvo = [0], t, nuvoStr;
     let i = 0;
     let res = {}
-    while ( i < size*2 ) {
+    while (i < size * 2) {
         //if (temp.length > 3) break;
         i++;
-        tm("increment", nuvo, 100,q0,0,false);
+        tm("increment", nuvo, 100, q0, 0, false);
         nuvoStr = nuvo.toLeftString();
         if (nuvoStr in arr) continue;
         t = compareMany(arr, nuvoStr, depth, aheads);
         if (t === 1) {
             arr.push(nuvoStr);
-        }
-        else // nuvo fails, log the cause
-        res[i] = "!!!Failed nuvo = " + nuvoStr + " caused by " + t;
+        } else // nuvo fails, log the cause
+            res[i] = "!!!Failed nuvo = " + nuvoStr + " caused by " + t;
     }
     console.log("buildDivGroup problems:", res);
     arr.push(memo['champ']);
     return [...arr];
 }
+
 //------------------------------------
 try {
     memo = require("../../_data/base1_size10_depth200.json");
-}
-catch {
+} catch {
     memo = {}
 }
-memo['champ'] = memo['champ'] ?? {steps:0, tape:""};
+memo['champ'] = memo['champ'] ?? {steps: 0, tape: ""};
 
-let divGroup = buildDivGroup(['0', '1'],10, depth = 200, aheads = 10);// base1_l10.txt
+let divGroup = buildDivGroup(['0', '1'], 40, depth = 200, aheads = 10);// base1_l10.txt
 let memoSorted = Object.keys(memo).sort(Array.compareTapes).map(val => val + ":" + memo[val]);
-memo.storeData(memo,"../../_data/base1_size10_depth200.json");
-memo.storeData(memoSorted,"../../_data/sorted-base1_size10_depth200.json");
+memo.storeData(memo, "../../_data/base1_size40_depth200.json");
+memo.storeData(memoSorted, "../../_data/sorted-base1_size40_depth200.json");
 divGroup['champ'] = memo['champ'];
-memo.storeData(divGroup,"../../_data/divGroup-base1_size10_depth200.json");
-console.log('champ = ',memo['champ'])
+memo.storeData(divGroup, "../../_data/divGroup-base1_size40_depth200.json");
+console.log('champ = ', memo['champ'])
 
 // console.log("Div group = ", divGroup, divGroup.length);
 
 // Result: Array(30), starting of '101' for 5 minutes work:
-arr =   ['101','1','01','11','001','011','111','1001','1101','0011','1011','1111',
-        '10001','11001','00101','11101','11011','10111','11111','100001','110001',
-        '101001','111001','000101','110101','101101','011101','111101','000011'];
-        nuvo = '100011';
+arr = ['101', '1', '01', '11', '001', '011', '111', '1001', '1101', '0011', '1011', '1111',
+    '10001', '11001', '00101', '11101', '11011', '10111', '11111', '100001', '110001',
+    '101001', '111001', '000101', '110101', '101101', '011101', '111101', '000011'];
+nuvo = '100011';
