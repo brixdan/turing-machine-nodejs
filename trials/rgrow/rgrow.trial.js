29b52bccad4503c47db9c1097e2c97904559f104
brixdan <brixdan@gmail.com>
2021-06-17 16:07:09 +0300

    Good for infinite [1,1,0,0,0,0,0,1,1]

--- trials/rgrow/rgrow.trial.js
+++ trials/rgrow/rgrow.trial.js
@@ -1,7 +1,13 @@
 const { tm, tmg } = require('../../engine');
 const fs = require('fs');
 let tape;
-
+function toFile(path,data) {
+    try {
+        fs.writeFileSync(path, data)
+    } catch (err) {
+        console.error(err)
+    }
+}
 +function () {
     temp = [];
     for (let i = 0; i < 104; i++) {
@@ -13,21 +19,16 @@ let tape;
 };
 +function () {
     tape = [1,0,1,1,0,1,1,0,1,0,1,1,1,0,1]//rgrow: stop at step 23595:
+    tape =     [1,1,0,0,0,0,0,1,1]
     let step = tm("rgrow", tape, 55000, q0, 0, false);
     console.log(" step = ", step, typeof tape);
 };
-function toFile(path,data) {
-    try {
-        fs.writeFileSync(path, data)
-    } catch (err) {
-        console.error(err)
-    }
-}
 
-+function () {
-    tape = [1,0,1]//rgrow: stop at step 31
-    tape = [1,0,1,1,0,1,1,0,1]//rgrow: stop at step 23595:
-    tape = [1,0,1,1,0,1,1,0,1,0,1,1,1,0,1]//rgrow: stop at step 23595:
+
++function visual () {
+    tape = [1,1,0,0,0,0,0,1,1]//rgrow: stop at step 31
+    //tape = [1,0,1,1,0,1,1,0,1]//rgrow: stop at step 23595:
+    //tape = [1,0,1,1,0,1,1,0,1,0,1,1,1,0,1]//rgrow: stop at step 23595:
     let l = tape.length;
     let d = { script: "rgrow", tape, p: 0, q: q0 };
     let it = tmg(d);
@@ -35,9 +36,9 @@ function toFile(path,data) {
     memo[d.p] = d.q;
     var html = '<link href="index.css" rel="stylesheet">' +
         '<div align="center"><table><th align="right">Negative</th>' +
-        '<td>Original</td><td>Positive</td><tbody>'
-    const limit = 50
-    while(true) {
+        '<td>Ori</td><td>Positive</td><tbody>\n'
+    const limit = 200
+    while(step < 1000) {
         let d = it.next().value;
         memo[d.p] = d.q;
         let str = '<th>';
@@ -49,19 +50,19 @@ function toFile(path,data) {
                     memo[i] === q1?'<b1>' + t + '</b1>':
                     memo[i] === q2?'<b2>' + t + '</b2>':
                                    '<b>' + t + '</b>';
-            str += (d.tape[i] !== undefined||i===d.p)? color:' ';
+            str += (d.tape[i] !== undefined||i===d.p)? color:'';
             if (i === -1) str += '</th><td bgcolor="#f5f5f5">'
             if (i === l-1) str += '</td><td>'
         }
         str += '</td>'
         html += '<tr>' + str + '</tr>\n'
         if (d.q === 'halt') {
-            html += `<tr><td></td><td>step = ${step}</td><td></td></tr>`;
             break;
         }
         step++;
     }
-    html +='</tbody></table></div>'
+    html +=`</tbody></table>\n
+            step = ${step}</div>`
     toFile("../../html/index.html",html);
     console.log(html)
     console.log("step = ", step)
