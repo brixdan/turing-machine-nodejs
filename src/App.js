a017dd81f815cd54bd87dd5be68c42aa28ad4718
brixdan <brixdan@gmail.com>
2021-06-20 22:07:51 +0300

    With in strict problem

--- src/App.js
+++ src/App.js
@@ -3,6 +3,50 @@ import { VariableSizeGrid as Grid } from "react-window";
 
 import "./styles.css";
 
+const { tm, tmg } = require('./engine');
+function visual () {
+    tape = [1,1,0,0,0,0,0,1,1]//rgrow: infinite
+    tape = [0,0,0,1]//rgrow: halt@46
+    //tape = [1,0,1,1,0,1,1,0,1]//rgrow: stop at step 23595:
+    //tape = [1,0,1,1,0,1,1,0,1,0,1,1,1,0,1]//rgrow: stop at step 23595:
+    let l = tape.length;
+    // let d = { script: "rgrow", tape, p: 0, q: q0 };
+    let d = { script: "rgrowim", tape, p: 0, q: q0 };
+    let it = tmg(d);
+    var memo = {}, step = 0;
+    memo[d.p] = d.q;
+    var html = '';
+    const limit = 200;
+    const infin = 20;
+    while(step < infin) {
+        let d = it.next().value;
+        memo[d.p] = d.q;
+        let str = '<th>';
+        let t = '';
+        let color = '';
+        for (let i = -limit; i < limit ; i++) {
+            t = `${i === d.p?'<u>'+(d.tape[i]??'*')+'</u>':d.tape[i]??''}`
+            let temp = {};
+            temp[q0] ='<b0>' + t + '</b0>';
+            temp[q1] ='<b1>' + t + '</b1>';
+            temp[q2] ='<b2>' + t + '</b2>';
+
+            color = temp[memo[i]]||'<b>' + t + '</b>';
+            str += (d.tape[i] !== undefined||i===d.p)? color:'';
+            if (i === -1) str += '</th><td>'
+            if (i === l-1) str += '</td><td>'
+        }
+        str += '</td>'
+        html += '<tr>' + str + '</tr>'
+        if (d.q === 'halt') {
+            break;
+        }
+        step++;
+    }
+    console.log(html)
+    console.log("step = ", step === infin?'infinity':step)
+}
+visual();
 // These cell sizes are arbitrary.
 // Yours should be based on the content of the cell.
 // const columnWidths = new Array(1000)
@@ -24,10 +68,8 @@ const Cell = ({ columnIndex, rowIndex, style }) => (
         // : "GridItemEven"
     }
     style={style}
-  >
-      { columnIndex === 0?rowIndex: 'r'+rowIndex}
-      { columnIndex === 0?'' : 'c'+columnIndex}
-  </div>
+    dangerouslySetInnerHTML={{ __html: columnIndex === 0?rowIndex:'<b1>Why</b1>' }}
+  ></div>
 );
 
 const App = () => (
---
+++ src/engine.js
@@ -0,0 +1,127 @@
+// My compact Turing Machine
+const colors = require('colors');
+const fs = require('fs');
+// Prepare symbols:
+[L, R, halt, q0, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, B, w, m, n] =
+    ["L", "R", "halt", "q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "B", "w", "m", "n"];
+
+Array.prototype.left = function(n) {
+    let r = [];
+    for (i = 0; i < this.length; i++) {
+        r[i - n] = this[i];
+    }
+    return r;
+};
+Array.prototype.toNumber = function toNumber() {
+    return +("0b" + this.join('').reverse());
+}
+Number.prototype.isPrime = function isPrime() {
+    if (isNaN(this) || !isFinite(this) || this%1 || this<2) return false;
+    var m=Math.sqrt(this); //returns the square root of the passed value
+    for (var i=2;i<=m;i++) if (this%i==0) return false;
+    return true;
+}
+Number.prototype.binaryNext = function binaryNext() {
+    return Math.pow(2,this.toString(2).length)
+}
+Number.prototype.toTape = function toTape() {
+    return this.toString(2).reverse().split('').map(Number);
+}
+Number.prototype.increase = function* increase() {
+    const str = this.toString(2).reverse();
+    const bn = this.binaryNext();
+    var i = 0;
+    while(true) {
+        // yield +('0b'+(str + i.toString(2).reverse()).reverse());
+        // this version just to ensure compatibility with MT
+        yield this + i*bn;
+        i++;
+    }
+    return;
+}
+Array.prototype.toLeftString = function(n = 100) {
+    let r = [];
+    if ((this['-1'] === undefined || this[-1] ==='B')) return this.join('');
+    for (i = 0; i < this.length + n + 1; i++) {
+        if (i - n === 0) r[i] = '*';
+        else if (i - n < 0) r[i] = this[i - n];
+        else if (this[i - n - 1] !== 'B') r[i + 1] = this[i - n - 1];
+    }
+    return r.join('');
+};
+
+String.prototype.toArray = function toArray () {
+    let r = [];
+    if (this.indexOf('*') > -1) {
+        for (let i = 0; i < this.length; i++)
+            if (i - this.indexOf('*') < 0)  r[i - this.indexOf('*')] = Number(this[i]);
+            else if (i - this.indexOf('*') > 0) r[i - this.indexOf('*') - 1] = Number(this[i]);
+        return r;
+    }
+    return Array.prototype.map.call(this, e => Number(e));
+}
+String.prototype.reverse = function reverse () {
+        return this.split("").reverse().join("");
+}
+Object.prototype.storeData = function storeData(data,path) {
+    try {
+        fs.writeFileSync(path, JSON.stringify(data))
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+Object.prototype.loadData = function loadData(path) {
+    try {
+        return fs.readFileSync(path, 'utf8')
+    } catch (err) {
+        console.error(err)
+        return false
+    }
+}
+
+Array.compareTapes = function compareTapes(x,y) {
+    let t = x.length - y.length;
+    if (t !== 0) return t; else
+    if (x.length > 0) {
+        t = x[x.length - 1] - y[y.length - 1];
+        if (t !== 0) return t; else {
+            x = x.slice(0, x.length - 1);
+            y = y.slice(0, y.length - 1);
+            return compareTapes(x, y)
+        }
+    }
+    return 0;
+}
+
+module.exports.tmg = function* tmg(d = {script:"", tape:[0], p:0, q: q0 }) {
+    let script = require("./TMs/" + d.script);
+    function step(d) {
+        let tape = Object.assign([], d.tape); p = d.p; q = d.q; // don't change income!!!
+        with (script[q][tape[p] ?? "B"]) {
+            tape[p] = w
+            q = n
+            switch (m) {        // move to next p-position
+                case L:
+                    p = p - 1;
+                    break;
+                case R:
+                    p = p + 1;
+                    break;
+                default:
+                    p = m; // or jump -)
+            }
+            return {tape, p, q}
+        }
+    }
+    let _d = {}; _d.tape = Object.assign([], d.tape);  _d.p = d.p; _d.q = d.q;
+    while (_d.q !== 'halt') {
+        yield _d;
+        _d = step(_d);
+    }
+    return _d;
+}
+
+
+
+
